b8483d932867d09787d3918281d0817a2af0f973
zuxbrt <zmuhovic97@gmail.com>
2019-08-26 15:33:21 +0200

    tables done

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    public function course()
+    {
+        return $this->hasMany('App\Course');
+    }
 }
---
+++ database/migrations/2014_10_12_000000_create_customers_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customersCourses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('customer_id');
+            $table->string('course_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customersCourses');
+    }
+}
