c6660fee5076a30edc28d2722f9059c70a93b6bf
zuxbrt <zmuhovic97@gmail.com>
2019-09-02 15:08:14 +0200

    Update pdf.blade.php

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -11,11 +11,11 @@
   <h1>PDF Document heading</h1>
   
   <table width="100%" style="width:100%" border="0">
-        <td>Ime: {{$customerData->name}}</td>
-        <td>Prezime: {{$customerData->surname}}</td>
-        <td>Email: {{$customerData->email}}</td>
-        <td>Telefon: {{$customerData->phone}}</td>
-        <td>Grad: {{$customerData->city}}</td>
+        Ime: {{$customerData->name}}
+        Prezime: {{$customerData->surname}}
+        Email: {{$customerData->email}}
+        Telefon: {{$customerData->phone}}</td>
+        Grad: {{$customerData->city}}</td>
         <td>Lice: {{$customerData->status}}</td>
         <td>Cijena: {{$customerData->fee}}</td>
         <td>Popust: {{$customerData->discount}}</td>
