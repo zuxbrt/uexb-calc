787cb83e3c6ff23cf28fec0ef0481d90f06071f5
Emir Mekić <emir.mekic@live.com>
2019-11-11 11:21:43 +0100

    novi tekst

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,60 +1,72 @@
 <html>
+
 <head>
 </head>
 
 <style>
-
-    body{
-        font-family: 'Work Sans',Helvetica,Arial,Lucida,sans-serif;
+    body {
+        font-family: 'Work Sans', Helvetica, Arial, Lucida, sans-serif;
         font-weight: 600;
     }
 
-    .centered{
+    .centered {
         text-align: center !important;
         margin-top: 10em;
         font-weight: 300;
     }
 
-    .back{
+    .centered-text {
+        text-align: center !important;
+        font-weight: 300;
+    }
+
+    .back {
         width: 100%;
         text-align: center;
     }
 
-    .button-back{
+    .button-back {
         background-color: #73ae56;
         width: 250px;
         height: 36px;
         border-radius: 25px;
-        -webkit-box-shadow: 0 1px 0 rgba(0,0,0,.1);
-        -moz-box-shadow: 0 1px 0 rgba(0,0,0,.1);
-        box-shadow: 0 1px 0 rgba(0,0,0,.1);
+        -webkit-box-shadow: 0 1px 0 rgba(0, 0, 0, .1);
+        -moz-box-shadow: 0 1px 0 rgba(0, 0, 0, .1);
+        box-shadow: 0 1px 0 rgba(0, 0, 0, .1);
         padding-top: 15px;
         cursor: pointer;
         margin-left: auto;
         margin-right: auto;
         margin-top: 2em;
-        transition: transform .2s; /* Animation */
+        transition: transform .2s;
+        /* Animation */
     }
 
-    .decorated{
+    .decorated {
         text-decoration: none;
         color: white;
         font-weight: 300;
         font-size: 18px;
     }
 
-    .button-back:hover{
+    .button-back:hover {
         transform: scale(1.05);
     }
-
 </style>
 
 <body>
-    <h1 class="centered">Predračun je kreiran i poslan na vašu e-mail adresu.</h1>
+    <h1 class="centered">Hvala vam na prijavi.</h1>
+    <p class="centered-text">
+        Kroz par minuta na vaš mail će bit generisan predračun sa instrukcijama za plaćanje.
+    </p>
+    <p class="centered-text">
+        Za bilo kakva pitanja budite slobodni da nam se javite na <a href="mailto: prijava@uciexcel.ba ">prijava@uciexcel.ba</a> ili <a href="tel:+38733956222">033/956-222</a>.
+    </p>
     <div class="back">
         <div class="button-back">
             <a class="decorated" href="https://uciexcel.ba">Nazad na UčiExcel.ba</a>
         </div>
     </div>
 </body>
+
 </html>
